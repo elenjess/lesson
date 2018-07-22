d55c6896f9a643d0956d04881898c776f7d701e4
elenjess <39803627+elenjess@users.noreply.github.com>
2018-07-22 18:55:45 +0300

    ДЗ (API)

--- api.php
+++ api.php
@@ -1,11 +1,8 @@
 <?php
-
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://dog.ceo/api/breeds/list/all");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-
 $list = json_decode(curl_exec($curl), TRUE);
-
 curl_setopt($curl, CURLOPT_URL, "https://dog.ceo/api/breeds/image/random");
 ?>
 	<ul>
@@ -13,12 +10,10 @@ curl_setopt($curl, CURLOPT_URL, "https://dog.ceo/api/breeds/image/random");
 		<? foreach ($list['message'] as $breed => $subbreeds) {
 			echo "<li>$breed</li>";
 			$image = json_decode(curl_exec($curl), TRUE);
-				echo "<li>".$image['message']."</li>";
-
+				echo "<img src=".$image['message'];
 		};?>
 
 
 	</ul>
-
 
 
